484b0e8af7e5a923639edc179b67caae9b4fbd34
Joshua Schijns <jmschijn@ucalgary.ca>
2017-02-10 09:33:02 -0700

    Working
    changed some things, game should work

--- TeamExercices/Week5/GuessingGame.java
+++ TeamExercices/Week5/GuessingGame.java
@@ -7,17 +7,22 @@ import java.util.ArrayList;
 public class GuessingGame {
   private int numToGuess;
   private int maxGuess = 10;
-  ArrayList <String>guesses = new ArrayList<>();
+  ArrayList <String> guesses = new ArrayList<>();
 
+// This method is public so it can be called outside of the class, is the main game loop
 public void play(){
    numToGuess = new Random().nextInt(maxGuess);
    boolean gameWon = false;
+   String playerGuess = "";
    while (!gameWon){
-     String playerGuess = getGuess();
-     gameWon = checkLastGuess();
+     playerGuess = getGuess();
+     if (checkLastGuess(playerGuess)){
+       gameWon = true;
+     }
    }
+   System.out.println("You guessed " + numToGuess + " correctly!");
 }
-
+  // Public so the user can set the maxGuess number
   public void setMaxGuess(int newMax){
       if (newMax >= 0){
         maxGuess = newMax;
@@ -28,6 +33,7 @@ public void play(){
 // Get user guesses method. Adds valid user guesses to the array list.
 // The while loop is acting as the inner loop and the for loop in the GuessingGameTest file is the outer loop.
 // The while loop is nested because the outer for loop in GuessingGameTest.java indirectly calls it
+// Private since this method is only called within the class by play()
   private String getGuess() {
     Scanner keyboard = new Scanner(System.in);
     String userGuess = "";
@@ -40,6 +46,7 @@ public void play(){
   return userGuess;
   }
 // Loop gets each element of the array and prints them.
+// Private since this method is only called within the class by play()
   private void printGuesses() {
     System.out.print("Your guesses: ");
     for (int counter=0 ;counter<guesses.size();counter++) {
@@ -47,15 +54,18 @@ public void play(){
       System.out.print(index+" ");
     }
   }
-  private boolean checkLastGuess() {
+  // Private since this method is only called within the class by play()
+  private boolean checkLastGuess(String playerGuess) {
     boolean lastGuess = false;
-    String lastGuessArray = guesses.get(-1);
-    int userGuessInt = Integer.parseInt(lastGuessArray);
+    //String lastGuessArray = guesses.get(-1);
+    //int userGuessInt = Integer.parseInt(lastGuessArray);
+    int userGuessInt = Integer.parseInt(playerGuess);
     if (userGuessInt == numToGuess) {
       lastGuess = true;
     }
     return lastGuess;
   }
+  // Private since this method is only called within the class, not sure where yet
   private double getAverageGuess() {
     int sum = 0;
     for (int counter=0 ;counter<guesses.size();counter++) {
@@ -66,6 +76,7 @@ public void play(){
     double average = sum/5;
     return average;
   }
+  // Private since this method is only called within the class by getGuess()
   private boolean isValidGuess(String userGuess) {
     boolean validGuess = false;
     int lengthOfGuess = userGuess.length();
