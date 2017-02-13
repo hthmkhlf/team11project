a7cbbc4b9a1cd23601dbe7d884da8c9e37b9c072
haidev12 <haiithemk@gmail.com>
2017-02-13 00:49:16 -0700

    Main class pretty much done
    - Added documentation
    - Added all the required features (not really sure if I am right or
    wrong)
    
    - Got rid of       // int Y_MIN = 0;
          // int Y_MAX = 2;
          // int X_MIN = 0;
    we do did not used them on our previous code. I just commented them so
    feel free to uncomment them if you thing that we may use them.
    
    * Need to check what you guys have as variable names so I can pass the
    right variables this explains why line 31 - 36 are not done.

--- classes/Game.java
+++ classes/Game.java
@@ -1,44 +1,74 @@
+/**
+* Author: Haithem Khelif
+* Team : 11
+* Last Update : 12/02/2017
+*
+* This class "Game.java" is the main class in order to
+* have the game running all the other classe should be
+* within the same directory.I made all the methods private
+* because other classes must serve this class and not vice versa.
+*/
+
+import java.util.Scanner;
+import java.util.Random;
 public class Game {
-    User user = new User;
-    Obstacle obstacle = new Obstacle;
-    Interface configuration = new Interface;
 
+/*
+* Create class instances and objects.
+*/
+    public User user = new User();
+    public Obstacle obstacle = new Obstacle();
+    public Interface configuration = new Interface();
+    public GameStats stats = new GameStats();
 
-    public void Setup() {
-      int Y_MIN = 0;
-      int Y_MAX = 2;
-      int X_MIN = 0;
+/* This Class will gather all the data needed from the other classes
+* Will not return anything
+*/
+    private void Setup() {
+      // int Y_MIN = 0;
+      // int Y_MAX = 2;
+      // int X_MIN = 0;
       boolean obstacle_hit = false;
-      int player_x_value = 0;
-      int player_y_value = 1;
+      int player_x_value = ;
+      int player_y_value = ;
+      int obstacle_y_value = ;
+      int obstacle_y_value1 = ;
+      int obstacle_y_value2 = ;
     }
 
-    public void Play() {
-      //Main loop
+/**
+* This method gets data from the Obstacle class and the User class.
+* Then checks if there is a collision between the player and the obstacle.
+* Returns boolean
+*/
+    private boolean collision(int player_x_value,int player_y_value) {
+      if (player_x_value == obstacle_x_value){
+        if ((player_y_value == obstacle_y_value1) || (player_y_value == obstacle_y_value2)) {
+            obstacle_hit = true;
+            System.out.println("You hit the obstacle");
+        }
+      }
+      // if user avoids the obstacle by jumping or sliding the player values will increase so they will fully pass the obstacle.
+      if ((player_input.equals("s")) || (player_input.equals("j"))) {
+        player_x_value= player_x_value +1;
+      }
+    return obstacle_hit;
+    }
+
+/**
+* Method acts like a main loop
+*/
+    private void Play(int player_x_value,int player_y_value,int obstacle_y_value,int obstacle_y_value1,int obstacle_y_value2) {
+
       while (obstacle_hit == false){
-          System.out.println("You are at x:" + player_x_value + " y:" + player_y_value);
-          System.out.print("The next object is at x:" +obstacle_x_value);
-          System.out.print(" y:" + obstacle_y_value1);
-          System.out.println("," + obstacle_y_value2);
-          System.out.println("Enter j to move up one x-block (jump over obstacle), s to move down one y-block (slide under an obstacle), any other key to just keep running");
+        System.out.println("You are at x:" + player_x_value + " y:" + player_y_value);
+        System.out.print("The next object is at x:" +obstacle_x_value);
+        System.out.print(" y:" + obstacle_y_value1);
+        System.out.println("," + obstacle_y_value2);
+        System.out.println("Enter j to move up one x-block (jump over obstacle), s to move down one y-block (slide under an obstacle), any other key to just keep running");
 
-          String player_input = keyboard.next();
-
-    }
-
-    public void collision() {
-    // Check if there is a collision between the player and the obstacle.
-                //check if player hits obstacle
-            if (player_x_value == obstacle_x_value){
-              if ((player_y_value == obstacle_y_value1) || (player_y_value == obstacle_y_value2)) {
-                  obstacle_hit = true;
-                  System.out.println("You hit the obstacle");
-              }
-            }
-            // if user avoids the obstacle by jumping or sliding the player values will increase so they will fully pass the obstacle.
-            if ((player_input.equals("s")) || (player_input.equals("j"))) {
-              player_x_value= player_x_value +1;
-            }
+        String player_input = keyboard.next();
 
     }
+  }
 }
