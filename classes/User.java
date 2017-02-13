4ae8d6d5b3ff8fc6efb8cbfc16f180e88d2286a9
haidev12 <haiithemk@gmail.com>
2017-02-12 22:55:19 -0700

    Fixed some errors in Max's User.java
    - Defined the arguments
    - Changed some variables names
    - Edited the levels of indentation
    ** We need to make sure that everyone have the same names for the
    instances variables.

--- classes/User.java
+++ classes/User.java
@@ -1,41 +1,42 @@
 import java.util.Scanner;
 
-public class User{	
+public class User{
 	 int player_x_value = 0;
 	 int player_y_value = 1;
 	 String player_name = "";
 	 Scanner keyboard = new Scanner(System.in);
 
-	 public movement(player_x_value,player_y_value,player_input){
-	  switch (player_input.toLowerCase())
-	  String player_input = keyboard.next();
+	 public String movement(int player_x_value,int player_y_value,String getPlayer_input){
+    String player_input = keyboard.next();
+    switch (player_input.toLowerCase()) {
 
-				  case "j":
-					  player_y_value = 2;
-					  player_x_value = player_x_value + 1;
-					  System.out.println("You jumped!");
-					  break;
+		  case "j":
+			  player_y_value = 2;
+			  player_x_value = player_x_value + 1;
+			  System.out.println("You jumped!");
+			  break;
 
-				  case "s":
-					  player_y_value = 0;
-					  player_x_value = player_x_value + 1;
-					  System.out.println("You slid!");
-					  break;
+		  case "s":
+			  player_y_value = 0;
+			  player_x_value = player_x_value + 1;
+			  System.out.println("You slid!");
+			  break;
 
-				  default:
-					  player_y_value = 1;
-					  player_x_value = player_x_value + 1;
-					  break;
-		return player_input
+		  default:
+			  player_y_value = 1;
+			  player_x_value = player_x_value + 1;
+			  break;
 	  }
+		return player_input;
+  }
 
-	 public getName(player_name){
+	 public String etName(String getPlayer_name){
 	  System.out.println("Welcome to the game. Please enter your name");
 
 			String player_name = keyboard.next();
 
 			System.out.println(player_name);
-		return player_name
+		return player_name;
 	 }
 
 }
