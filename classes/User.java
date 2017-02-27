c6744dfd72ea964dee30697d47702555b21f86ab
haidev12 <haiithemk@gmail.com>
2017-02-26 23:10:58 -0700

    User Class
    fixed some errors

--- classes/User.java
+++ classes/User.java
@@ -1,48 +1,27 @@
+/**
+ * @author Dou, Zhi Chao
+ *
+*/
 import java.util.Scanner;
 
 public class User{
-	 private int player_x_value = 0;
-	 private int player_y_value = 1;
-	 private String player_name = "";
-	 Scanner keyboard = new Scanner(System.in);
+	public void movement(){
+			System.out.print("Movement: ");
+			//calls the location class
+			Location userMovement = new Location(0,1);
+			// userMovement.Location(0,1);
+			Scanner keyboard = new Scanner(System.in);
+  		String movement = keyboard.nextLine();
+  		String movementUser = movement.toLowerCase();
 
-	 public String movement(){
-    String player_input = keyboard.next();
-    switch (player_input.toLowerCase()) {
-
-		  case "j":
-			  player_y_value = 2;
-			  player_x_value = player_x_value + 1;
-			  System.out.println("You jumped!");
-			  break;
-
-		  case "s":
-			  player_y_value = 0;
-			  player_x_value = player_x_value + 1;
-			  System.out.println("You slid!");
-			  break;
-
-		  default:
-			  player_y_value = 1;
-			  player_x_value = player_x_value + 1;
-				System.out.println("You moved!");
-			  break;
-	  }
-		return player_input;
-  }
-
-	 public String getName(){
-	  System.out.println("Welcome to the game. Please enter your name");
-		String player_name = keyboard.next();
-		return player_name;
-	 }
-
-	 public int getPlayer_x_value() {
-	 	return player_x_value;
-	 }
-
-	 public int getPlayer_y_value() {
-	 	return player_y_value;
-	 }
-
+				if (movementUser.equals("w")){
+					//moves the user position up
+					userMovement.setY(0);
+					System.out.println("You jumped!");
+				}else if (movementUser.equals("s")){
+					//moves the user position down
+					userMovement.setY(2);
+					System.out.println("You slide!");
+				}
+	}
 }
