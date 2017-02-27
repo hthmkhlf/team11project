ae0bf9b3a69ee3a30245b1f73dae70502dc43242
Jawgo <jmschijn@ucalgary.ca>
2017-02-26 23:55:49 -0700

    Grid fixes
    I have added in more to the grid class, the drawGrid will draw the
    stuff, adjustGrid will remove old locations

--- classes/Grid.java
+++ classes/Grid.java
@@ -1,23 +1,33 @@
-// I plan to add more, will use the location class to grab the location
-// of each thing, obstacle and player to add into the grid
-// A note, 0,0 is the top left corner, as you go down y increases
-// and for the coordinates of the grid in [][], it goes [y][x]
+/**
+* I plan to add more, will use the location class to grab the location
+* of each thing, obstacle and player to add into the grid
+* A note, 0,0 is the top left corner, as you go down y increases
+* and for the coordinates of the grid in [][], it goes [y][x]
+* @author Josh Schijns
+*/
 
-class Grid{
+public class Grid{
     private static final char PLAYER = 'P';
     private static final char OBSTACLE = 'X';
     private static final char EMPTY = ' ';
-
     // The initital grid
-    private static char [][] startConfig ={
+    private char [][] startConfig ={
         {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
         {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
         {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}
     };
 
-    // This method will draw the grid out
-    public void drawGrid(){
-        char [][] grid = startConfig;
+    private char [][] grid = startConfig;
+
+    /**
+    * This method will draw the grid out
+    * @param playerX is the X coordinate of the player
+    * @param playerY is the Y coordinate of the player
+    */
+    public void drawGrid(int playerX,int playerY){
+        grid [playerY][playerX] = PLAYER;
+        //grid [][] = OBSTACLE;
+        //grid [][] = OBSTACLE;
         for (int row = 0; row <3; row++){
             System.out.print("|");
             for (int column = 0; column < 10; column++){
@@ -27,5 +37,14 @@ class Grid{
         }
     }
 
+    public void adjustGrid(){
+        for (int row = 0; row <3; row++){
+            for (int column = 0; column < 10; column++){
+                if (grid [row][column] == PLAYER){
+                    grid [row][column] = EMPTY;
+                }
+            }
+        }
+    }
 
 }
