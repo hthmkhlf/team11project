c6c57e93f5b75d19778e7ae36936cb5fcc059b43
emily-f <emily.flanagan@ucalgary.ca>
2017-02-27 00:01:47 -0700

    uses actual user score

--- classes/HighScoreMain.java
+++ classes/HighScoreMain.java
@@ -3,7 +3,7 @@
 public class HighScoreMain{
   public static void main(String[] args){
     HighScore highScore = new HighScore();
-    int userNewScore = 0;
+//     int userNewScore = UserScore.gamehasended();
     int currentHighScore = highScore.previousHighScore();
     boolean isHigher = highScore.compareScores(currentHighScore, userNewScore);
     if (isHigher){
