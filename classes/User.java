b08142918f9978297fd6c581cbb078f9dcf9b062
Haithem Khelif <haiithemk@gmail.com>
2017-02-27 10:45:06 -0700

    Update User.java

--- classes/User.java
+++ classes/User.java
@@ -5,23 +5,32 @@
 import java.util.Scanner;
 
 public class User{
+	private Location userMovement = new Location(0,1);
+	private Scanner keyboard = new Scanner(System.in);
 	public void movement(){
 			System.out.print("Movement: ");
 			//calls the location class
-			Location userMovement = new Location(0,1);
 			// userMovement.Location(0,1);
-			Scanner keyboard = new Scanner(System.in);
   		String movement = keyboard.nextLine();
   		String movementUser = movement.toLowerCase();
 
 				if (movementUser.equals("w")){
 					//moves the user position up
-					userMovement.setY(0);
+					userMovement.setY(-1);
+					userMovement.setX(1);
 					System.out.println("You jumped!");
 				}else if (movementUser.equals("s")){
 					//moves the user position down
-					userMovement.setY(2);
+					userMovement.setY(1);
+					userMovement.setX(1);
 					System.out.println("You slide!");
+				}else {
+					userMovement.setX(1);
+					System.out.println("You are running");
 				}
+	}
+
+	public Location getLocation(){
+		return userMovement; 
 	}
 }
