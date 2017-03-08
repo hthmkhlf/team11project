c998d67e081cf01d843e6c8e2319cd26ac9084cf
K8E2 <K8E2@users.noreply.github.com>
2017-03-08 12:28:23 -0700

    Update Obstacle.java

--- classes/Obstacle.java
+++ classes/Obstacle.java
@@ -7,42 +7,39 @@ import java.util.Random;
  */
 
 public class Obstacle {
-  private int obstacle_x_value = 4;
-  private int obstacle_y_value = 1;
+  private int obstacle_x_value = 0;
+  private int obstacle_y_value = 0;
   private int distanceFromPlayer = 0;
+  private Location obstacleLocation = new Location(obstacle_x_value, obstacle_y_value);
 
-  private Location obstacle1Location = new Location(obstacle_x_value, obstacle_y_value);
-  private Location obstacle2Location = new Location(obstacle_x_value, obstacle_y_value);
+  public Obstacle(int x, int y) {
+    obstacle_x_value = x;
+    obstacle_y_value = y;
+  }
 
   /**
-   * This method checks if the player has successfully passed the last obstacle and if so
-   * generates the x-coordinate for a new obstacle.
-   * The distance from the player is random, but it will always be 2-3 spaces in front of them.
+   * This method checks generates the x-coordinate for a new obstacle.
+   * The distance from the player is random, but it will always be 1-2 spaces in front of them.
    *
    * @param player_x_value the user's x-coordinate which is used to check if the player has
    * passed the last obstacle and determine the location of the next obstacle
    */
   public void generateTopX(int player_x_value) {
-    if (player_x_value > obstacle_x_value) {
-      distanceFromPlayer = new Random().nextInt(2) + 2;
-      obstacle_x_value = player_x_value + distanceFromPlayer;
-      obstacle1Location.setX(obstacle_x_value);
-    }
+    distanceFromPlayer = new Random().nextInt(2) + 1;
+    obstacle_x_value = player_x_value + distanceFromPlayer;
+    obstacleLocation.setX(obstacle_x_value);
   }
 
   /**
-   * This method checks if the player has successfully passed the last obstacle and if so
-   * generates the y-coordinate for a new obstacle.
+   * This method generates the y-coordinate for a new obstacle.
    * This coordinate will either be 0 or 1.
    *
    * @param player_x_value the user's x-coordinate which is used to check if the player has
    * passed the last obstacle
    */
-  public void generateTopY(int player_x_value) {
-    if (player_x_value > obstacle_x_value) {
-      obstacle_y_value = new Random().nextInt(2);
-      obstacle1Location.setY(obstacle_y_value);
-    }
+  public void generateTopY() {
+    obstacle_y_value = new Random().nextInt(2);
+    obstacleLocation.setY(obstacle_y_value);
   }
 
   /**
@@ -53,7 +50,7 @@ public class Obstacle {
    */
   public void generateBottomX(Obstacle other) {
     obstacle_x_value = other.obstacle_x_value;
-    obstacle2Location.setX(obstacle_x_value);
+    obstacleLocation.setX(obstacle_x_value);
   }
 
   /**
@@ -66,13 +63,23 @@ public class Obstacle {
    */
   public void generateBottomY(Obstacle other) {
     obstacle_y_value = other.obstacle_y_value + 1;
-    obstacle2Location.setY(obstacle_y_value);
+    obstacleLocation.setY(obstacle_y_value);
   }
 
-  public int getLocationX(){
-    return obstacle1Location.getXCoord();
+  public Obstacle getObstacle() {
+    return new Obstacle(obstacle_x_value, obstacle_y_value);
   }
+
+  /**
+   * @return the x-coordinate of the obstacle.
+   */
+  public int getLocationX(){
+    return obstacleLocation.getXCoord();
+  }
+  /**
+   * @return the y-coordinate of the obstacle.
+   */
   public int getLocationY(){
-    return obstacle1Location.getYCoord();
+    return obstacleLocation.getYCoord();
   }
 }
