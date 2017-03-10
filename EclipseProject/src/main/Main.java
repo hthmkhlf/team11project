f5633ae3beea817cffad6a899c8c99ec0abfc81d
Haithem Khelif <haiithemk@gmail.com>
2017-03-10 00:28:51 -0700

    Game frame

---
+++ EclipseProject/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // Setting the frame environment
+        JFrame frame = new JFrame("Team 11 Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(750, 350);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setAlwaysOnTop(true);
+    }
+
+}
