668b5710704a770041e249eb7ab642e58a7e7a28
Haithem Khelif <haiithemk@gmail.com>
2017-03-12 17:46:04 -0600

    Added key listeners for jump and slide

--- EclipseProject/src/main/GameWindow.java
+++ EclipseProject/src/main/GameWindow.java
@@ -1,4 +1,4 @@
-//package main;
+package main;
 /**
 *@author Josh Schijns
 */
@@ -19,6 +19,7 @@ public class GameWindow extends JPanel implements KeyListener{
 
     // image for png's, had BufferedImage here, will use again later down the road
     private Image image;
+    private Image player;
     private Graphics2D graphics;
 
     public GameWindow(){
@@ -34,12 +35,13 @@ public class GameWindow extends JPanel implements KeyListener{
         // read image
         try {
         	// TODO change the path for the file, figure out how to shorten it in eclipse, make sure to put the double \ due to it being an escape code
-            image = ImageIO.read(new File("C:\\Users\\Josh\\Documents\\School\\CPSC233\\team11project\\EclipseProject\\src\\images\\background.png"));
+            image = ImageIO.read(new File("src\\images\\background.png"));
+            player = ImageIO.read(new File("src\\images\\player1.png"));
         }   catch (IOException e ) {
         	e.printStackTrace();
         }
         graphics.drawImage(image, 0, 0,this.getWidth(),this.getHeight(), null);
-        graphics.drawRect(10,10,100,100);
+        graphics.drawImage(player, 120, 304,150,150, null);
         //repaint(); // use once we have a timer set up, will call this paint again
     }
 
@@ -49,7 +51,11 @@ public class GameWindow extends JPanel implements KeyListener{
     }
     public void keyPressed(KeyEvent key) {
 
-      System.out.println("Hi");
+    	if (key.getKeyCode() == KeyEvent.VK_UP ) {
+    		System.out.println("You Jumped !");
+    	}else if (key.getKeyCode() == KeyEvent.VK_DOWN) {
+    		System.out.println("You Slided !");
+    	}
 
     }
     public void keyReleased(KeyEvent key) {
