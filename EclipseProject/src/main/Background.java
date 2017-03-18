8be25076aaf6b06a979dce5541eac33d486e9e5a
Joshua Schijns <jmschijn@ucalgary.ca>
2017-03-18 10:10:25 -0600

    Added a Background class
    add the first layer for background, right now it moves at a constant rate and replicates itself so it is repeating

---
+++ EclipseProject/src/main/Background.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.*;
+
+public class Background extends MapObject {
+	private Image image;
+	private double rate = 2;
+	
+	public Background(){
+		super(0,0,550,1700);
+		try {
+        	// TODO change the path for the file, figure out how to shorten it in eclipse, make sure to put the double \ due to it being an escape code
+            image = ImageIO.read(new File("src/images/layer1.png"));
+        }catch (IOException e ) {
+        	e.printStackTrace(); // this will print errors to the console
+        }
+	}
+		
+	
+	@Override
+	protected void draw(Graphics2D graphics) {
+        graphics.drawImage(image, getXCoord(), getYCoord(),getWidth(),getHeight(), null);
+        graphics.drawImage(image, getXCoord() + getWidth(), getYCoord(),getWidth(),getHeight(), null);
+        if (getXCoord() < -(getWidth())){
+        	setX(0);
+        }
+	}
+
+	@Override
+	public void update() {
+		movement();
+
+	}
+
+	@Override
+	public void movement() {
+		setX((int)(getXCoord() - rate));
+	}
+
+}
