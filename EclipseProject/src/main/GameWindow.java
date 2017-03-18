e109713ba0ffc72ecc1a80b383a7fb011ede3769
Joshua Schijns <jmschijn@ucalgary.ca>
2017-03-18 09:53:44 -0600

    GameWindow
    Instead of repainting I created a thread and used Runnable to run it at 30FPS

--- EclipseProject/src/main/GameWindow.java
+++ EclipseProject/src/main/GameWindow.java
@@ -1,76 +1,119 @@
 package main;
-/**
-*@author Josh Schijns
-*/
-import java.awt.*;
-//import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.awt.event.*;
-import javax.swing.Timer;
-
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
 
-public class GameWindow extends JPanel implements KeyListener{
-
-    // dimensions for the window
-    public static final int WINDOW_WIDTH = 320;
-    public static final int WINDOW_HEIGHT = 240;
-
-    // image for png's, had BufferedImage here, will use again later down the road
-    private Image image;
-//    private Image player;
-    private Graphics2D graphics;
-    private User player = new User();
-
-    public GameWindow(){
-        super();
-        setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
-        setFocusable(true);
-        requestFocus();
-        addKeyListener(this);
-        // This creates a timer for repainting, so in essence will paint everything every 200ms
-        Timer timer = new Timer(200,
-                new ActionListener(){
-                    public void actionPerformed(ActionEvent e){
-                        repaint();
-                    }
-                });
-          timer.setInitialDelay(1000);
-          timer.start();
-    }
-
-    public void paint(Graphics graphics){
-        super.paint(graphics);
-        // read image
-        try {
-        	// TODO change the path for the file, figure out how to shorten it in eclipse, make sure to put the double \ due to it being an escape code
-            image = ImageIO.read(new File("src\\images\\background.png"));
-        }   catch (IOException e ) {
-        	e.printStackTrace(); // this will print errors to the console
-        }
-        graphics.drawImage(image, 0, 0,this.getWidth(),this.getHeight(), null);
-        player.drawPlayer(graphics);
-      
-    }
-
-
-    public void keyTyped(KeyEvent key) {
-
-    }
-    public void keyPressed(KeyEvent key) {
-
-    	if (key.getKeyCode() == KeyEvent.VK_UP ) {
-    		System.out.println("You Jumped !");
-    		player.moveUp();
-    	}else if (key.getKeyCode() == KeyEvent.VK_DOWN) {
-    		System.out.println("You Slided !");
-    	}
-
-    }
-    public void keyReleased(KeyEvent key) {
-
-    }
-
+@SuppressWarnings("serial")
+public class GameWindow extends JPanel implements Runnable, KeyListener{
+	// dimensions for the window
+	public static int WINDOW_WIDTH = 1650;
+	public static int WINDOW_HEIGHT = 550;
+	
+	// images
+	private Graphics2D graphics;
+	private BufferedImage image;
+	private Player player = new Player();
+	private Background background = new Background(); // maybe set up a manager to do this
+	
+	private int FPS = 30;
+	private double averageFPS;
+	
+	
+	
+	private Thread thread;
+	private boolean running;
+	
+	public GameWindow(){
+		super();
+		setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+		setFocusable(true);
+		requestFocus();
+	}
+	
+	public void addNotify(){
+		super.addNotify();
+		if(thread == null){
+			thread = new Thread(this);
+			thread.start();
+		}
+		addKeyListener(this);
+	}
+	
+	public void run(){
+		running = true;
+		
+		image = new BufferedImage(WINDOW_WIDTH, WINDOW_HEIGHT, BufferedImage.TYPE_INT_RGB);
+		graphics = (Graphics2D) image.getGraphics();
+		//used for establishing time
+		long startTime;
+		long elapsedTime;
+		long waitTime;
+		long totalTime = 0;
+		int frameCount = 0;
+		int maxFrameCount = 30;
+		long targetTime = 1000 / FPS;
+		
+		
+		while(running){
+			startTime = System.nanoTime();
+			
+			gameUpdate();
+			gameRender();
+			gameDraw();
+			// This is used to calculate the time that has elapsed since starting the timer,
+			// the wait time is used to put the thread to sleep so it lasts the targetTime.
+			// The total time keeps track of how long it takes to run through 30 frames and establishes an
+			// average rate. 
+			elapsedTime = (System.nanoTime() - startTime) / 1000000;
+			waitTime = targetTime - elapsedTime;
+			if(waitTime < 0){
+				waitTime = 0;
+			}
+			try{
+				Thread.sleep(waitTime);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			totalTime += System.nanoTime() - startTime;
+			frameCount++;
+			if(frameCount == maxFrameCount){
+				averageFPS = 1000.0 / ((totalTime / frameCount) / 1000000);
+				frameCount = 0;
+				totalTime = 0;
+			}
+			
+		}
+	}
+	
+	private void gameUpdate(){
+		//updating everthing
+		player.update();
+		background.update();
+	}
+	
+	private void gameRender(){
+		//draw everything to offscreen so it is loaded
+		super.paint(graphics);
+		
+		background.draw(graphics);
+		graphics.drawString("FPS: "+ averageFPS, 10, 10);
+		player.draw(graphics);
+		
+	}
+	
+	private void gameDraw(){
+		//drawing on the screen
+		Graphics graphics2 = this.getGraphics();
+		graphics2.drawImage(image, 0, 0, null);
+		graphics2.dispose();
+	}
+	
+	public void keyTyped(KeyEvent key){}
+	public void keyPressed(KeyEvent key){
+		if(key.getKeyCode() == KeyEvent.VK_UP){
+			player.toggleJump();
+		}
+	}
+	public void keyReleased(KeyEvent key){}
 }
