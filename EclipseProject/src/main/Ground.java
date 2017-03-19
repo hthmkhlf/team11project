103891ece96c2494851995a1aede0b4aa798f5a3
Joshua Schijns <jmschijn@ucalgary.ca>
2017-03-19 12:00:58 -0600

    Created Ground.java
    This the ground for the game

---
+++ EclipseProject/src/main/Ground.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
+import java.awt.*;
+import javax.imageio.ImageIO;
+/**
+* @author Josh Schijns
+* This class controls the ground, drawing and updating movement.
+*/
+public class Ground extends MapObject {
+	private Image image;
+	
+	public Ground(){
+		super(0,0,525,1650);
+		try {
+            image = ImageIO.read(new File("src/images/layer2.png"));
+        }catch (IOException e ) {
+        	e.printStackTrace(); // this will print errors to the console
+        }
+	}
+
+	@Override
+	public void draw(Graphics2D graphics) {
+		// TODO Auto-generated method stub
+		
+		graphics.drawImage(image, getXCoord(),getYCoord(),getWidth(),getHeight(),null);
+		graphics.drawImage(image, getXCoord()+ getWidth(),getYCoord(),getWidth(),getHeight(),null);
+		if (getXCoord() < -(getWidth())){
+			setX(0);
+		}
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		movement();
+	}
+
+	@Override
+	protected void movement() {
+		// TODO Auto-generated method stub
+		setX((int)(getXCoord() - scrollRate));
+	}
+
+}
