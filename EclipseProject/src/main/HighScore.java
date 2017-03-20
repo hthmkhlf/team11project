863124ffb66157589fa4bfc2c5c47abfd7e3d930
emily-f <emily.flanagan@ucalgary.ca>
2017-03-20 10:51:07 -0600

    Add files via upload

--- EclipseProject/src/main/HighScore.java
+++ EclipseProject/src/main/HighScore.java
@@ -2,58 +2,60 @@ import java.io.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+*@author Emily Flanagan
+*@since 2017-03-20
+*/
+
 public class HighScore{
-  private int userNewScore = 0;
   private boolean gameInProgress = true;
   private int userScore = 0;
   public int currentHighScore = 0;
   private Timer score = new Timer();
 
+/**
+*/
   public void highScoreBoard() {
-  String fileName = "HighScoreBoard.txt";
-  String line = null;
+    String fileName = "HighScoreBoard.txt";
+    String line = null;
 
-  try{
-    FileReader fileReader = new FileReader(fileName);
-    BufferedReader bufferedReader = new BufferedReader(fileReader);
+    try{
+      FileReader fileReader = new FileReader(fileName);
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-    while((line = bufferedReader.readLine()) != null){
-      System.out.println(line);
+      while((line = bufferedReader.readLine()) != null){
+        System.out.println(line);
+      }
+      bufferedReader.close();
     }
-    bufferedReader.close();
+    catch(FileNotFoundException ex){
+      System.out.println("Unable to open file");
+    }
+    catch(IOException ex){
+      System.out.println("Error reading file");
+    }
   }
-  catch(FileNotFoundException ex){
-    System.out.println("Unable to open file");
-  }
-  catch(IOException ex){
-    System.out.println("Error reading file");
-  }
-}
 
-
-//user score
-//runs a timertask to use as score keeping
+/**
+* defines the event for Timer; adds 1 point to user score every second
+*/
   TimerTask count = new TimerTask() {
     public void run(){
       userScore++;
     }
   };
-  //used from game class when game has ended timer is stopped and user score is passed through
-  public int gameHasEnded(){
-    gameInProgress = false;
-    score.cancel();
-    System.out.println("UserScore " + userScore);
-    return userScore;
-  }
-  //starts timertask
+
+/**
+* Starts the timer
+*/
   public void start() {
     score.scheduleAtFixedRate(count, 0, 3000);
   }
 
 //high score validation
 /**
-*writes in the previous high score from the file HighScores.txt
-*to be used with HighScoreMain and Game class
+* Finds the previous high score from the file HighScores.txt
+* Used by Game class
 */
   public int previousHighScore() {
     String fileName = "HighScores.txt";
@@ -80,13 +82,13 @@ public class HighScore{
   }
 
 /**
-*takes the currentHighScore from the game calss and compares it to the one written
-*in from the file
+* Takes the currentHighScore from the game class and compares it to the one
+* written in from the file. Called by runHighScore.
 */
   public boolean compareScores(){
     boolean isHigher = false;
-    System.out.println("Your score is " + this.userNewScore);
-    if (this.userNewScore > this.currentHighScore){
+    System.out.println("Your score is " + this.userScore);
+    if (this.userScore > this.currentHighScore){
       isHigher = true;
     }else{
       isHigher = false;
@@ -95,15 +97,15 @@ public class HighScore{
   }
 
 /**
-changes score in file to the new score if applicable
+* Changes score in HighScores.txt file to the new score, if applicable
 */
-  public void writeInNewScore(int userNewScore){
+  public void writeInNewScore(){
     String fileName = "HighScores.txt";
     BufferedWriter bWriter = null;
     FileWriter fWriter = null;
 
     try{
-      String writeIn = String.valueOf(userNewScore);
+      String writeIn = String.valueOf(this.userScore);
       fWriter = new FileWriter(fileName);
       bWriter = new BufferedWriter(fWriter);
       bWriter.write(writeIn);
@@ -115,20 +117,19 @@ changes score in file to the new score if applicable
       }
   }
 
-// implements high score
-//takes score from game class to update user score
-  public void setUserNewScore( int newScore){
-    userNewScore = newScore;
-  }
-//takes user score passes it into the highscore class
-//compares the two.
-  public void runHighScore( int userNewScore){
-    HighScore highScore = new HighScore();
+/**
+* Called by Game class when collision occurs (game over);
+* Calls compareScores. Prints the high score to screen and
+* records it in file for next game occurance.
+*/
+  public void gameHasEnded(){
+    gameInProgress = false;
+    score.cancel();
     currentHighScore = highScore.previousHighScore();
     boolean isHigher = highScore.compareScores();
     if (isHigher){
       System.out.println("You got the new high score!");
-      highScore.writeInNewScore(userNewScore);
+      highScore.writeInNewScore();
     }
     else{
       System.out.println("You did not get a new high score.");
