c7ec6db9020b3c931e1fc216392cd3d2934e3cfd
Joshua Schijns <jmschijn@ucalgary.ca>
2017-03-22 09:21:31 -0600

    update

--- EclipseProject/src/state/PlayState.java
+++ EclipseProject/src/state/PlayState.java
@@ -2,84 +2,120 @@ package state;
 
 import java.awt.event.KeyEvent;
 import java.awt.Graphics2D;
-import map.Background;
-import map.Ground;
-import map.HighScoreGUI;
-import map.Obstacle;
-import map.Player;
-
+import map.*;
+/**
+ * 
+ * @author Josh Schijns
+ * This is the state of the game when the user is playing the actual Game
+ *
+ */
 public class PlayState {
-	// All stuff commented out is me trying to figure out a way to spawn multiple obstacles
-	
+	private boolean collision = false;
 	private Player player;
-//	private Obstacle[] obstacle; 
+	private Obstacle[] obstacle; 
 	private Background background;
 	private Ground ground;
 	private HighScoreGUI score;
-	private int difficulty = 1;
-//	private Obstacle obstacle;
-//	private int frameCount = 0;
-//	private int obstacleNumber = 0;
+	private int difficulty = 2;
 
+	/**
+	 * Default constructor, initializes all the map objects and spawns the first set of obstacles
+	 */
 	public PlayState(){
 		player = new Player();
-		obstacle = new Obstacle[5];
+		obstacle = new Obstacle[difficulty];
 		background = new Background();
 		ground = new Ground();
 		score = new HighScoreGUI();
-//		spawnObstacles();
-//		obstacle = new Obstacle();
-		
+		createObstacles();		
 	}
+	
+	
 	public void update(){
-		player.update();
-		background.update();
-		ground.update();
-//		for (int count = 0; count < 5; count++){
-//			obstacle[count].update();
-//		}
-		score.update();
-//		obstacle.update();
-//		spawn();
-	}
-	
-	public void draw(java.awt.Graphics2D graphics){
-		background.draw(graphics);
-		ground.draw(graphics);		
-//		for (int count = 0; count < 5; count++){
-//			obstacle[count].draw(graphics);
-//		}
-		player.draw(graphics);
-		score.draw(graphics);
-//		obstacle.draw(graphics);
-		
-		
-	}
-	
-	public void keyPressed(int key){
-		if(key == KeyEvent.VK_UP){
-			player.toggleJump();
+		if(!collision){
+			player.update();
+			background.update();
+			ground.update();
+			for (int count = 0; count < difficulty; count++){
+				obstacle[count].update();
+			}
+			score.update();
+			spawnObstacles();
 		}
 	}
 	
-//	public void spawnObstacles(){
-//		int distanceBetween = 1700 / difficulty; 
-//		for(int count = 0; count < 5; count++){
-//			obstacle[count] = new Obstacle((int)1700+(count*distanceBetween));
-//			
-//		}		
-//	}
+	public void draw(Graphics2D graphics){
+		background.draw(graphics);
+		ground.draw(graphics);		
+		for (int count = 0; count < difficulty; count++){
+			obstacle[count].draw(graphics);
+		}
+		player.draw(graphics);
+		score.draw(graphics);		
+	}
 	
-//	public void spawn(){
-//		frameCount++;
-//		if((frameCount/120) == 1){
-//			obstacle = new Obstacle();
-//			frameCount = 0;
-//			obstacleNumber++;
-//			if (obstacleNumber == 2){
-//				obstacleNumber = 0;
-//			}
-//		}
-//	}
-
+	/**
+	 * Makes the player jump
+	 * @param key is the key pressed, if UP then will make player jump.
+	 */
+	public void keyPressed(int key){
+		if(key == KeyEvent.VK_UP){
+			player.toggleJump();
+			player.setSpeed(true);
+		}
+	}
+	
+	/**
+	 * Toggles the speed variable so the player no longer gains jump speed
+	 * @param key is the key pressed, releasing UP will only do something.
+	 */
+	public void keyReleased(int key){
+		if (key == KeyEvent.VK_UP){
+			player.setSpeed(false);
+		}
+	}
+	
+	/**
+	 * Based off the difficulty will spawn that many objects within the screen
+	 */
+	private void createObstacles(){
+		int distanceBetween = 1700 / difficulty; 
+		for(int count = 0; count < difficulty; count++){
+			obstacle[count] = new Obstacle((int)1700+(count*distanceBetween));
+			
+		}		
+	}
+	
+	/**
+	 * Based off the difficulty it will keep track of each obstacle and 
+	 * re spawn it if it goes off the screen.
+	 */
+	private void spawnObstacles(){
+		for(int count = 0; count < difficulty; count++){
+			if (isCollided(obstacle[count])){
+				collision = true;
+				System.out.println("You have collided with an Obstacle!!");
+			}
+			if (obstacle[count].getXCoord() < -obstacle[count].getWidth()){
+				obstacle[count] = new Obstacle(1700);
+			}
+		}	
+	}
+	
+	private boolean isCollided(Obstacle other){
+		boolean collided = false;
+		int startX = 70;
+		int endX = 125;
+		int startY = player.getYCoord();
+		int endY = player.getYCoord() + 80;
+		if (((startX >= other.getXCoord()) && (startX <= other.getXCoord()+other.getWidth()))
+				|| ((endX >= other.getXCoord())&&(endX <= (other.getXCoord()+other.getWidth())))){
+			if (((startY >= other.getYCoord())&&(startY <= (other.getYCoord()+other.getHeight())))
+					|| ((endY >= other.getYCoord())&&(endY <= (other.getYCoord()+other.getHeight())))){
+				collided = true;
+			}
+		}
+		return collided;
+	}
+	
 }
