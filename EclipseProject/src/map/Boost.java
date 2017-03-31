f7bce9d20d869cf2c664a1447f4ccf6b7a34018d
K8E2 <K8E2@users.noreply.github.com>
2017-03-30 21:35:43 -0600

    Update Boost.java

--- EclipseProject/src/map/Boost.java
+++ EclipseProject/src/map/Boost.java
@@ -1,4 +1,4 @@
-//Call this class when obstacle3 is created
+//call this class when obstacle3 is created
 
 package map;
 import java.awt.Graphics2d;
@@ -9,7 +9,7 @@ import java.awt.*;
 
 /**
  * @author Katie Tieu
- * This class is a child class to MapObject           //May need to change if Collectable class is made
+ * This class is a child class to MapObject           //may need to change if Collectable class is made
  * The methods in this class will be used to generate collectable objects which give the
  * player a height boost when jumping.
  */
@@ -18,17 +18,11 @@ public class Boost extends MapObject{
   private Image image;
 
   public Boost(){
-    this(1600, 300, 80, 80);   //may need to change this coordinate
+    this(1600, 300, 80, 80);   //may need to change coordinates/size
   }
 
   public Boost(Obstacle obs){
     this(obs.getXCoord() - 200, 300, 80, 80);
-
-    try{
-      image = ImageIO.read(new File("src/image/boost.png"));  //may need to change file name
-    }catch (IOException e){
-      e.printStackTrace();
-    }
   }
 
   public Boost(int newX, int newY, int newHeight, int newWidth){
@@ -36,6 +30,30 @@ public class Boost extends MapObject{
     setY(newY);
     setHeight(newHeight);
     setWidth(newWidth);
+    try{
+      image = ImageIO.read(new File("src/image/boost.png"));  //may need to change file name
+    }catch (IOException e){
+      e.printStackTrace();  // This will print errors to the console
+    }
+  }
+
+  @Override
+  public void draw(Graphics2D graphics){
+    graphics.drawImage(image, getXCoord(), getYCoord(), getWidth(), getHeight());
+  }
+
+  @Override
+  public void update(){       //Get rid of this later, I just put it in for now
+    movement();
+  }
+
+  @Override
+  public void movement(){
+    setX((int)(getXCoord() - getScroll()));
+  }
+
+  public void boostUp(){
+    
   }
 
 }
