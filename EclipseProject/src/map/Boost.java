e9d2975729b95123516999dba16c2515b299ca31
K8E2 <K8E2@users.noreply.github.com>
2017-04-02 18:11:25 -0600

    tenatively done

--- EclipseProject/src/map/Boost.java
+++ EclipseProject/src/map/Boost.java
@@ -1,7 +1,7 @@
 //call this class when obstacle3 is created
 
 package map;
-import java.awt.Graphics2d;
+import java.awt.Graphics2D;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -9,37 +9,42 @@ import java.awt.*;
 
 /**
  * @author Katie Tieu
- * This class is a child class to MapObject           //may need to change if Collectable class is made
+ * This class is a child class to the Collidable class.
  * The methods in this class will be used to generate collectable objects which give the
  * player a height boost when jumping.
  */
 
-public class Boost extends MapObject{
+public class Boost extends Collidable{
   private Image image;
+  private double boostedJumpSpeed = -15;      // Needs to be adjusted
+  private double fallSpeed = 1.5;
+  private double distance;
+  private final int GROUND = 349;
 
   public Boost(){
-    this(1600, 300, 80, 80);   //may need to change coordinates/size
+    this(1600, 300, 60, 60);   //may need to change coordinates/size
   }
 
-  public Boost(Obstacle obs){
-    this(obs.getXCoord() - 200, 300, 80, 80);
+  public Boost(Obstacle obstacle){
+    this(obstacle.getXCoord() - 300, 300, 60, 60);
   }
 
   public Boost(int newX, int newY, int newHeight, int newWidth){
-    setX(newX);
-    setY(newY);
-    setHeight(newHeight);
-    setWidth(newWidth);
+    super(newX, newY, newHeight, newWidth);
     try{
-      image = ImageIO.read(new File("src/image/boost.png"));  //may need to change file name
+      image = ImageIO.read(new File("src/image/boost.png"));
     }catch (IOException e){
       e.printStackTrace();  // This will print errors to the console
     }
   }
 
+  public Boost(Boost other){
+    super(other.getXCoord(), other.getYCoord(), other.getHeight(), other.getWidth());
+  }
+
   @Override
   public void draw(Graphics2D graphics){
-    graphics.drawImage(image, getXCoord(), getYCoord(), getWidth(), getHeight());
+    graphics.drawImage(image, getXCoord(), getYCoord(), getWidth(), getHeight(), null);
   }
 
   @Override
@@ -52,8 +57,16 @@ public class Boost extends MapObject{
     setX((int)(getXCoord() - getScroll()));
   }
 
-  public void boostUp(){
-    
+  // Copied code from movement() in Player
+  // Takes player from boost upwards and then down to ground
+  // Should merge with Player.movement() at some point to make jumping boolean false
+  @Override
+  public void collisionAction(Player player){
+    for(int timeChange = 0; player.getYCoord() > GROUND;){
+      distance = boostedJumpSpeed + (fallSpeed * timeChange);
+      player.setY((int)(getYCoord() + distance));
+      timeChange++;
+    }
   }
 
 }
