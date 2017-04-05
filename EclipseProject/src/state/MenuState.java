4759968bfd8cc6f3db0ebe6ed55a73c541e66d29
Jawgo <jmschijn@ucalgary.ca>
2017-04-04 22:33:37 -0600

    small fix, varibales have to be lowercase at start

--- EclipseProject/src/state/MenuState.java
+++ EclipseProject/src/state/MenuState.java
@@ -2,72 +2,49 @@ package state;
 
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import main.Manager;
 
 import javax.imageio.ImageIO;
 
+import main.Manager;
+
 public class MenuState {
 //	 BackGround image
-	private BufferedImage image,Start,Scores,Credits,Exit,KeyMap;
+	private BufferedImage image,start,scores,credits,exit,keyMap;
 	private Manager manager;
 
 	//Initiate Game Status
 	
 	public MenuState(Manager manager){		
+
 		try {
 			image = ImageIO.read(new File("src/images/background.png"));
-	
+			start = ImageIO.read(new File("src/images/startGameIcon.png"));
+			scores = ImageIO.read(new File("src/images/Scores.png"));
+			credits = ImageIO.read(new File("src/images/Credits.png"));
+			exit = ImageIO.read(new File("src/images/endGameIcon.png"));
+			keyMap = ImageIO.read(new File("src/images/KeyMap.png"));
 		}catch (IOException e ) {
 			e.printStackTrace();
 		}
-				
-		try {
-			Start = ImageIO.read(new File("src/images/startGameIcon.png"));
-		}catch (IOException e ) {
-			e.printStackTrace();
-		}
-	
-	
-		try {
-			Scores = ImageIO.read(new File("src/images/Scores.png"));
-		}catch (IOException e ) {
-			e.printStackTrace();
-		}
-	
-		try {
-			Credits = ImageIO.read(new File("src/images/Credits.png"));
-		}catch (IOException e ) {
-			e.printStackTrace();
-		}
-		
-		try {
-			Exit = ImageIO.read(new File("src/images/endGameIcon.png"));
-		}catch (IOException e ) {
-			e.printStackTrace();
-		}
-		
-		try {
-			KeyMap = ImageIO.read(new File("src/images/KeyMap.png"));
-		}catch (IOException e ) {
-			e.printStackTrace();
-		}
-		
 		this.manager = manager;
 	
 	
 	}
+	
+	public void update(){}
 
 
 	public void draw(Graphics2D graphics) {
 	    graphics.drawImage(image, 0, 0,1650, 550, null);      
-	    graphics.drawImage(Start, 600, 0, null);  
-	    graphics.drawImage(Scores, 600, 100, null);
-	    graphics.drawImage(Credits, 600, 200, null);
-	    graphics.drawImage(Exit, 600, 300, null);
-	    graphics.drawImage(KeyMap, 100, 250,null);
+	    graphics.drawImage(start, 600, 0, null);  
+	    graphics.drawImage(scores, 600, 100, null);
+	    graphics.drawImage(credits, 600, 200, null);
+	    graphics.drawImage(exit, 600, 300, null);
+	    graphics.drawImage(keyMap, 100, 250,null);
 
 	}
 	
