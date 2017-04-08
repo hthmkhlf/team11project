4240b40f3b971ca1f7f733f8c77e3fcc584878c6
Haithem Khelif <haiithemk@gmail.com>
2017-04-08 00:11:55 -0600

    Credits Done
    Took me some time to have the best positioning of the text. There is a
    better way to have them centered but don't wonna use JApplet may cause
    some issues with the other sub menu's.

--- EclipseProject/src/state/CreditsState.java
+++ EclipseProject/src/state/CreditsState.java
@@ -5,10 +5,13 @@ import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
 
@@ -17,33 +20,51 @@ import main.Main;
 
 public class CreditsState extends Main {
 	private Manager manager;
+	private BufferedImage CreditsBackGround;
 	private String teamMembers[];
 	private String titles[];
-	private Font namesFont;
+	private String sources[];
+	private Font textFont;
 	private Font titlesFont;
+	private Color aColor = Color.decode("#3A8EF2");
 
 
 
 	public CreditsState(Manager manager) {
 		//Arrays of multiple strings
 		teamMembers = new String[] { "Dou, Zhi Chao", "Flanagan, Emily", "Haithem Khelif","Josh Schijns","Katie Tieu"}; //Names are sorted alphabetically
-		titles = new String[] {"Team Members","About The Used Images"};
+		titles = new String[] {"Credits","Team Members","Art / Sounds"};
+		sources = new String[] {"freesound.org","opengameart.org"};
 		// Set Font Used and Import Images If required
-		namesFont = new Font ("Courier New", 1, 20);
-		titlesFont = new Font ("Courier New", 2, 25);
+		textFont = new Font ("Courier New", 1, 20);
+		titlesFont = new Font ("SansSerif.bold", 1, 50);
+		
+		try {
+			CreditsBackGround = ImageIO.read(new File("src/images/background.png"));
+		}catch (IOException e ) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void draw(Graphics2D graphics) {
 //		graphics.setBackground(Color.BLACK);
-		graphics.setColor(Color.BLACK);
-		graphics.fillRect(0, 0, 1650, 550);
+		graphics.drawImage(CreditsBackGround, 0,0,1650, 550, null);  
+		graphics.setColor(aColor);
 		graphics.setFont(titlesFont);
-		
-		
-		graphics.setFont (namesFont);
+		graphics.drawString(titles[0],700, 50);
+		graphics.drawString(titles[1],630, 140);
+		graphics.setFont (textFont);
 		for (int i=0; i < 5; i++) {
-			graphics.setColor(Color.WHITE);
-			graphics.drawString(teamMembers[i],775, 175+ i*25);
+			graphics.setColor(Color.BLACK);
+			graphics.drawString(teamMembers[i],720, 190+ i*25);
+			}
+		graphics.setColor(aColor);
+		graphics.setFont(titlesFont);
+		graphics.drawString(titles[2],650, 350);
+		graphics.setFont(textFont);
+		for (int i=0; i <= 1; i++) {
+			graphics.setColor(Color.BLACK);
+			graphics.drawString(sources[i],700, 400+ i*25);
 			}
 
 	}
