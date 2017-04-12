28df84eb6d5551a5ab5fde59307fb343b8c9d792
K8E2 <K8E2@users.noreply.github.com>
2017-04-11 23:59:25 -0600

    clean up

--- EclipseProject/src/map/Ground.java
+++ EclipseProject/src/map/Ground.java
@@ -5,34 +5,34 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+
 /**
- * Sets up and draws the ground for the map
  * @author Josh Schijns
- *
+ * This class is a child class to MapObject.
+ * This methods in this class draw the ground for the map.
  */
+
 public class Ground extends MapObject {
 	private BufferedImage image;
-	/**
-	 * Default constructor, calls MapObject to set it's values and loads the image
-	 */
 
-	public Ground(){
+	public Ground() {
 		this(0);
 	}
-	public Ground(int xCoord){
+	
+	public Ground(int xCoord) {
 		super(xCoord,0,525,1650);
 		try {
             image = ImageIO.read(new File("src/images/layer2.png"));
-		}catch(IllegalArgumentException iae){
+		} catch(IllegalArgumentException iae) {
 			ErrorMessage.addError("Image is null Ground");
-		}catch (IOException ioe) {
+		} catch (IOException ioe) {
 			ErrorMessage.addError("Error reading image for layer2");
 		}
 	}
 
 	@Override
 	public void draw(Graphics2D graphics) {		
-		graphics.drawImage(image, getXCoord(),getYCoord(),getWidth(),getHeight(),null);
+		graphics.drawImage(image, getXCoord(), getYCoord(), getWidth(), getHeight(), null);
 	}
 
 	@Override
