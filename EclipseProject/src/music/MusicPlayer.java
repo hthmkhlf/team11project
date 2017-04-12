d0376254d3bdce42fdb37c0cbd7ddf97ccbf3e0d
K8E2 <K8E2@users.noreply.github.com>
2017-04-12 00:55:10 -0600

    clean up

--- EclipseProject/src/music/MusicPlayer.java
+++ EclipseProject/src/music/MusicPlayer.java
@@ -1,58 +1,75 @@
 package music;
 
+import map.ErrorMessage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import map.ErrorMessage;
+/**
+ * @author Josh Schijins
+ * This class is used to control music in the game.
+ */
 
 public class MusicPlayer {
 	private Clip clip;
 	
-	public MusicPlayer(String fileName){
-		try{
+	public MusicPlayer(String fileName) {
+		try {
 			File file = new File(fileName);
-			if(file.exists()){
+			if(file.exists()) {
 				clip = AudioSystem.getClip();
 				AudioInputStream inputStream = AudioSystem.getAudioInputStream(file.toURI().toURL());
 				clip.open(inputStream);
 			}
-		}catch(LineUnavailableException lue){
+		} catch(LineUnavailableException lue) {
 			ErrorMessage.addError("clip not available due to resource restrictions");
-		}catch (SecurityException se) {
+		} catch (SecurityException se) {
 			ErrorMessage.addError("Clip not available, security reasons");
-		}catch(IllegalArgumentException iae){
+		} catch(IllegalArgumentException iae) {
 			ErrorMessage.addError("Clip not supported");
-		}catch(UnsupportedAudioFileException usafe){
+		} catch(UnsupportedAudioFileException usafe) {
 			ErrorMessage.addError("URL does not point to valid audio file");
-		}catch(IOException ioe){
+		} catch(IOException ioe) {
 			ErrorMessage.addError("I/O error MusicPlayer");
 		}
 	}
 	
-	
-	public void play(boolean toLoop){
-		clip.setFramePosition(0);  // Must always rewind!
-		if(toLoop){
+	/**
+	 * This method rewinds the audio clip.
+	 * @param toLoop if true will start the clip over.
+	 */
+	public void play(boolean toLoop) {
+		clip.setFramePosition(0);
+		if(toLoop) {
 			loop();
 		}
-        clip.start();
+		clip.start();
 	}
-	public void loop(){
+	
+	/**
+	 * This method loops the clip infinitely (until manually stopped).
+	 */
+	public void loop() {
 		clip.loop(Clip.LOOP_CONTINUOUSLY);
-    }
-      
-    public boolean playing(){
-    	return clip.isActive();
-    }
-      
-      
-    public void stop(){
-        clip.stop();
-    }
+	}
+
+	/**
+	 * This method is used to determine whether the audio clip is playing.
+	 * @return clip.isActive() checks whether the clip is in use or not.
+	 */
+	public boolean playing() {
+		return clip.isActive();
+	}
+
+	/**
+	 * This method stops playing the clip.
+	 */
+	public void stop() {
+		clip.stop();
+	}
+
 }
