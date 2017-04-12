e923035f4a0c045ba78ee4542c072fd1d7297542
K8E2 <K8E2@users.noreply.github.com>
2017-04-11 23:34:16 -0600

    clean up

--- EclipseProject/src/map/Background.java
+++ EclipseProject/src/map/Background.java
@@ -1,40 +1,36 @@
 package map;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 
 /**
- * Sets up the background for the game and scrolls at a slower rate
  * @author Josh Schijns
- *
+ * This class sets up the background for the game and scrolls at a slower rate.
  */
+
 public class Background extends MapObject {
 	private BufferedImage image;
 	private double rate = 2;
 	
-	/**
-	 * Default constructor, calls MapObject to set it's values and loads the image
-	 */
-	public Background(){
-		super(0,0,550,1700);
+	public Background() {
+		super(0, 0, 550, 1700);
 		try {
             image = ImageIO.read(new File("src/images/layer1.png"));
-		}catch(IllegalArgumentException iae){
-    			ErrorMessage.addError("Image is null in Background");
-        }catch (IOException ioe) {
-        	ErrorMessage.addError("Error reading image for layer1");
+		} catch(IllegalArgumentException iae){
+    			ErrorMessage.addError("Image is null in Background.");
+        } catch (IOException ioe) {
+        	ErrorMessage.addError("Error reading image for layer1.");
         }
 	}
-		
-	
+
 	@Override
 	public void draw(Graphics2D graphics) {
         graphics.drawImage(image, getXCoord(), getYCoord(),getWidth(),getHeight(), null);
         graphics.drawImage(image, getXCoord() + getWidth(), getYCoord(),getWidth(),getHeight(), null);
-        if (getXCoord() < -(getWidth())){
+        if (getXCoord() < -(getWidth())) {
         	setX(0);
         }
 	}
