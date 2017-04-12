2fb69359fc214e6e4f3065378db4e447c06542a8
K8E2 <K8E2@users.noreply.github.com>
2017-04-12 01:05:49 -0600

    documentation

--- EclipseProject/src/state/GameState.java
+++ EclipseProject/src/state/GameState.java
@@ -2,6 +2,12 @@ package state;
 
 import java.awt.Graphics2D;
 
+/**
+ * @author Josh Schijins
+ * This interface contains methods which will respectively switch between and draw the different game states based on
+ * user input (key press).
+ */
+
 public interface GameState {
 	public void update();
 	public void draw(Graphics2D graphics);
