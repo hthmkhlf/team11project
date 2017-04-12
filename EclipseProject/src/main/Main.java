155ce604057fb3e712ca96f0de838afd891e1fed
Haithem Khelif <haidev12@users.noreply.github.com>
2017-04-12 01:54:27 -0600

    Changed Frame name to Happy Bird

--- EclipseProject/src/main/Main.java
+++ EclipseProject/src/main/Main.java
@@ -4,24 +4,24 @@ import javax.swing.JFrame;
 
 /*
  * @author : Haithem Khelif
- * This is the main class. The game is run from here.
+ * Since 2017-04-12
+ * This is the main class. Where the frame is created.
  */
-
 public class Main {
 	public static void main(String[] args) {
-	    //Create the instance variables needed to run the game
+    	/* Create the instance variables needed to run the game */
 	    GameWindow gameWindow = new GameWindow();
 	
 	    // Setting the frame environment
-	    JFrame frame = new JFrame("Team 11 Game");
+	    JFrame frame = new JFrame("Happy Bird");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setSize(1650, 550);
 	    frame.setLocationRelativeTo(null);
 	    frame.setResizable(false);
 	    frame.setAlwaysOnTop(false);
-	   
 	    frame.setVisible(true);
-	    frame.setContentPane(gameWindow);
-	}
+		frame.setContentPane(gameWindow);
 
+     
+	}
 }
