882185f6d6353e4d56bd4a6c7577c16bb9d27967
K8E2 <K8E2@users.noreply.github.com>
2017-04-12 01:10:31 -0600

    removed commented lines and added @Override

--- EclipseProject/src/state/MenuState.java
+++ EclipseProject/src/state/MenuState.java
@@ -1,62 +1,60 @@
 package state;
 
+import main.Manager;
+import music.MusicPlayer;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-//import javax.swing.JTextField;
 
-import main.Manager;
-import music.MusicPlayer;
+/**
+ * @author Josh Schijins and Haithem Khelif
+ * This class controls the main menu from which the player can play the game, look at credits,
+ * look at high scores, or exit the game.
+ */
 
 public class MenuState implements GameState {
-//	 BackGround image
-	private BufferedImage image,start,scores,credits,exit,keyMap,keyArrow;
+	private BufferedImage image,start,scores,credits,exit,keyMap;
 	private Manager manager;
 	private MusicPlayer music;
-
-	//Initiate Game Status
 	
-	public MenuState(Manager manager){		
-
+	public MenuState(Manager manager) {
 		try {
 			image = ImageIO.read(new File("src/images/background.png"));
 			start = ImageIO.read(new File("src/images/startGameIcon.png"));
 			scores = ImageIO.read(new File("src/images/Scores.png"));
 			credits = ImageIO.read(new File("src/images/Credits.png"));
-			exit = ImageIO.read(new File("src/images/title1.png"));
-			keyMap = ImageIO.read(new File("src/images/keyMapNew.png"));
-		}catch (IOException e ) {
+			exit = ImageIO.read(new File("src/images/endGameIcon.png"));
+			keyMap = ImageIO.read(new File("src/images/KeyMap.png"));
+		} catch(IOException e ) {
 			e.printStackTrace();
 		}
 		this.manager = manager;
 		music = new MusicPlayer("src/music/BirdRunFullnew.wav");
 		music.play(true);
-		
 	}
 	
-	
+	@Override
 	public void update(){
 		if(!(music.playing())){
 			music.play(true);
 		}
 	}
 
-
+	@Override
 	public void draw(Graphics2D graphics) {
-		graphics.drawImage(image, 0, 0,1650, 550, null);      
-	    graphics.drawImage(start, 650, 100, null);  
-	    graphics.drawImage(scores, 650, 200, null);
-	    graphics.drawImage(credits, 650, 300, null);
-	    graphics.drawImage(exit, 540, 0, null);
-	    graphics.drawImage(keyMap, 600, 130,null);
-
+	    graphics.drawImage(image, 0, 0,1650, 550, null);      
+	    graphics.drawImage(start, 600, 0, null);  
+	    graphics.drawImage(scores, 600, 100, null);
+	    graphics.drawImage(credits, 600, 200, null);
+	    graphics.drawImage(exit, 600, 300, null);
+	    graphics.drawImage(keyMap, 100, 250,null);
 
 	}
 	
+	@Override
 	public void keyPressed(int key){
 		if(key == KeyEvent.VK_P){
 			music.stop();
@@ -71,5 +69,4 @@ public class MenuState implements GameState {
 		
 	}
 	
-
 }
