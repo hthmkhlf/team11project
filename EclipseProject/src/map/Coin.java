3f91dd31c0cec61221d6491fc75d8830751b4d78
K8E2 <K8E2@users.noreply.github.com>
2017-04-11 23:48:03 -0600

    clean up

--- EclipseProject/src/map/Coin.java
+++ EclipseProject/src/map/Coin.java
@@ -4,36 +4,38 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Random;
-
 import javax.imageio.ImageIO;
-
 import music.MusicPlayer;
 
+/**
+ * @author Haithem Khelif
+ * This class is a child class to MapObject.
+ * The methods in this class are used to create and manage collectable coins which are
+ * counted as the player collects them. 
+ */
+
 public class Coin extends Collidable {
-	
+	private Random random = new Random();
+	private MusicPlayer music;
 	private BufferedImage[] coin = new BufferedImage[8];
 	private int frameNum = 0;
-	private static int coinsCollected = 0;
 	private boolean collected = false;
-	private MusicPlayer music;
+	private static int coinsCollected = 0;
 	private static final int MAX_Y = 435;
 	private static final int MIN_Y = 100;
 	private static final int MAX_X = 2300;
 	private static final int MIN_X = 1800;
-	private Random random = new Random();
 	
-	
-	
-	public Coin(){
+	public Coin() {
 		super(1800,200,45,48);
 		setX(random.nextInt(MAX_X-MIN_X + 1) + MIN_X);
 		setY(random.nextInt(MAX_Y-MIN_Y + 1) + MIN_Y);
 		for(int frame = 0; frame < 8; frame++)
 			try {
 				coin[frame] = ImageIO.read(new File("src/images/coin" +(frame+1) + ".png"));
-			}catch(IllegalArgumentException iae){
+			} catch(IllegalArgumentException iae){
     			ErrorMessage.addError("Image is null in Coin");
-			}catch (IOException ioe) {
+			} catch (IOException ioe) {
 				ErrorMessage.addError("Error reading image for coin");
 			}
 		music = new MusicPlayer("src/soundEffects/coinSound.wav");
@@ -46,14 +48,13 @@ public class Coin extends Collidable {
 			music.play(false);
 			coinsCollected++;
 		}
-
 	}
 
 	@Override
 	public void draw(Graphics2D graphics) {
 		if(!collected){
 			int num = frameNum / 5;
-			graphics.drawImage(coin[num], getXCoord(),getYCoord(),getWidth(),getHeight(),null);
+			graphics.drawImage(coin[num], getXCoord(), getYCoord(), getWidth(), getHeight(), null);
 			frameNum++;
 			if (frameNum == 40){
 				frameNum = 0;
@@ -67,11 +68,18 @@ public class Coin extends Collidable {
 
 	}
 	
+	/**
+	 * This method returns the number of coins the player collected during the game.
+	 * @return coinsCollected is the number of coins collected.
+	 */
 	public int getCollectedcoins() {
 		return coinsCollected;
 		
 	}
 	
+	/**
+	 * This method sets the number of coins collected to 0.
+	 */
 	public static void resetCoin() {
 		coinsCollected = 0;
 	}
